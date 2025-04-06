fec8ffd61cb1504c7c057bb1bfffa9ca5f4218e0
DFR-Repo <36865485+DFR-Repo@users.noreply.github.com>
2025-04-06 22:11:13 +0300

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,6 +1,6 @@
 const https = require("https");
 
-https.get("https://your-railway-app-url.up.railway.app/", (res) => {
+https.get("https://test-production-93f1.up.railway.app/", (res) => {
   console.log(`📡 Ping sent: ${res.statusCode}`);
 }).on("error", (e) => {
   console.error(`❌ Ping error: ${e.message}`);
